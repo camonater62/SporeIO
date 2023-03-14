0183b664719f7109c55087633a7eace53163204e
Cameron West <camonater62@gmail.com>
2023-03-14 12:55:04 -0700

    Static universe

--- universe.js
+++ universe.js
@@ -1,4 +1,4 @@
-let starColors = [
+const starColors = [
   "#FFFFFF",
   "#FFFFD9",
   "#FFFFA3",
@@ -9,11 +9,20 @@ let starColors = [
   "#9D1928",
 ];
 
+
+
 class StarSystem {
+  /**
+   * 
+   * @param {Number} x 
+   * @param {Number} y 
+   * @param {Boolean} generateFullSystem 
+   * @returns 
+   */
   constructor(x, y, generateFullSystem = false) {
     this.Lehmer = ((x & 0xffff) << 16) | (y & 0xffff);
 
-    this.starExists = this.rndInt(0, 20) == 1;
+    this.starExists = this.rndInt(0, 200) == 1;
     if (!this.starExists) return;
 
     this.starDiameter = this.rndDouble(40, 150);
@@ -43,7 +52,7 @@ class StarSystem {
       p.gases *= sum;
       p.water *= sum;
 
-      p.populate = max(this.rndInt(-50000000, 20000000), 0); // 1/5 chance for no pop
+      p.population = max(this.rndInt(-50000000, 20000000), 0); // 1/5 chance for no pop
 
       p.ring = this.rndInt(0, 10) == 1;
 
@@ -70,14 +79,101 @@ class StarSystem {
     return m2;
   }
 
+  /**
+   * 
+   * @param {Number} min 
+   * @param {Number} max 
+   * @returns Number
+   */
   rndInt(min, max) {
     return Number(this.Lehmer32() % BigInt(max - min)) + min;
   }
 
+  /**
+   * 
+   * @param {Number} min 
+   * @param {Number} max 
+   * @returns Number
+   */
   rndDouble(min, max) {
     let acc = 1000000;
     let double = Number(this.Lehmer32() % BigInt(acc)) / acc;
     return double * (max - min) + min;
+  }
+}
+
+class Universe {
+  /**
+   * 
+   * @param {Number} w 
+   * @param {Number} h 
+   */
+  constructor(w, h) {
+    this.w = w;
+    this.h = h;
+
+    this.grid = [];
+
+    for (let y = 0; y < h; y++) {
+      this.grid[y] = [];
+      for (let x = 0; x < w; x++) {
+        this.grid[y][x] = new StarSystem(x, y, true);
+      }
+    }
+  }
+
+  draw() {
+    let sectorsX = width / 16;
+    let sectorsY = height / 16;
+  
+    let mouse = createVector(int(mouseX / 16), int(mouseY / 16));
+    let galaxy_mouse = createVector(
+      int(mouse.x + galaxyOffset.x),
+      int(mouse.y + galaxyOffset.y)
+    );
+  
+    let screen_sector = createVector(0, 0);
+  
+    for (screen_sector.y = 0; screen_sector.y < sectorsY; screen_sector.y++) {
+      for (screen_sector.x = 0; screen_sector.x < sectorsX; screen_sector.x++) {
+        let star = this.grid[screen_sector.y][screen_sector.x];
+  
+        if (star.starExists) {
+          noStroke();
+          fill(star.starColor);
+          circle(
+            screen_sector.x * 16 + 8,
+            screen_sector.y * 16 + 8,
+            star.starDiameter / 8
+          );
+  
+          if (mouse.x == screen_sector.x && mouse.y == screen_sector.y) {
+            stroke("orange");
+            noFill();
+            circle(
+              screen_sector.x * 16 + 8,
+              screen_sector.y * 16 + 8,
+              star.starDiameter / 4
+            );
+          }
+        }
+      }
+    }
+  
+    if (mouseIsPressed) {
+      let star = this.grid[galaxy_mouse.y][galaxy_mouse.x];
+  
+      if (star.starExists) {
+        starSelected = galaxy_mouse;
+      } else {
+        starSelected = undefined;
+      }
+    }
+  
+    if (starSelected != undefined) {
+      let star = this.grid[starSelected.y][starSelected.x];
+      drawWindow(star);
+    }
   }
 }
 
@@ -96,79 +192,39 @@ class Planet {
   }
 }
 
+/**
+ * @type {p5.Vector}
+ */
 let galaxyOffset;
+/**
+ * @type {p5.Vector}
+ */
 let starSelected;
+
+let universe;
 
 function setupUniverse() {
   galaxyOffset = createVector(0, 0);
   starSelected = undefined;
+  universe = new Universe(width / 16, height / 16);
 }
 
 function drawUniverse() {
-  let dt = deltaTime / 1000;
-  if (keyIsDown(UP_ARROW)) galaxyOffset.y -= 50 * dt;
-  if (keyIsDown(DOWN_ARROW)) galaxyOffset.y += 50 * dt;
-  if (keyIsDown(LEFT_ARROW)) galaxyOffset.x -= 50 * dt;
-  if (keyIsDown(RIGHT_ARROW)) galaxyOffset.x += 50 * dt;
+  // let dt = deltaTime / 1000;
+  // if (keyIsDown(UP_ARROW)) galaxyOffset.y -= 50 * dt;
+  // if (keyIsDown(DOWN_ARROW)) galaxyOffset.y += 50 * dt;
+  // if (keyIsDown(LEFT_ARROW)) galaxyOffset.x -= 50 * dt;
+  // if (keyIsDown(RIGHT_ARROW)) galaxyOffset.x += 50 * dt;
 
   background(0);
-
-  let sectorsX = width / 16;
-  let sectorsY = height / 16;
-
-  let mouse = createVector(int(mouseX / 16), int(mouseY / 16));
-  let galaxy_mouse = createVector(
-    int(mouse.x + galaxyOffset.x),
-    int(mouse.y + galaxyOffset.y)
-  );
-
-  let screen_sector = createVector(0, 0);
-
-  for (screen_sector.y = 0; screen_sector.y < sectorsX; screen_sector.y++) {
-    for (screen_sector.x = 0; screen_sector.x < sectorsX; screen_sector.x++) {
-      let star = new StarSystem(
-        screen_sector.x + galaxyOffset.x,
-        screen_sector.y + galaxyOffset.y
-      );
-
-      if (star.starExists) {
-        noStroke();
-        fill(star.starColor);
-        circle(
-          screen_sector.x * 16 + 8,
-          screen_sector.y * 16 + 8,
-          star.starDiameter / 8
-        );
-
-        if (mouse.x == screen_sector.x && mouse.y == screen_sector.y) {
-          stroke("orange");
-          noFill();
-          circle(
-            screen_sector.x * 16 + 8,
-            screen_sector.y * 16 + 8,
-            star.starDiameter / 4
-          );
-        }
-      }
-    }
-  }
-
-  if (mouseIsPressed) {
-    let star = new StarSystem(galaxy_mouse.x, galaxy_mouse.y);
-
-    if (star.starExists) {
-      starSelected = galaxy_mouse;
-    } else {
-      starSelected = undefined;
-    }
-  }
-
-  if (starSelected != undefined) {
-    let star = new StarSystem(starSelected.x, starSelected.y, true);
-    drawWindow(star);
-  }
+  universe.draw();
+  
 }
 
+/**
+ * 
+ * @param {StarSystem} star 
+ */
 function drawWindow(star) {
   // Draw window
   noStroke();
